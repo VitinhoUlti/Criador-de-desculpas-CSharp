0157084fcd94ea92a51d684b2aff72c1d49bd8c4
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-05-28 22:42:17 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Desculpas_de_Bryan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Pasta = new System.Windows.Forms.Button();
+            this.Salvar = new System.Windows.Forms.Button();
+            this.Abrir = new System.Windows.Forms.Button();
+            this.Aleatorio = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desculpa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descrição:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usado em:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ultimo usado:";
+            // 
+            // Pasta
+            // 
+            this.Pasta.Location = new System.Drawing.Point(15, 130);
+            this.Pasta.Name = "Pasta";
+            this.Pasta.Size = new System.Drawing.Size(75, 23);
+            this.Pasta.TabIndex = 4;
+            this.Pasta.Text = "Pasta";
+            this.Pasta.UseVisualStyleBackColor = true;
+            this.Pasta.Click += new System.EventHandler(this.Pasta_Click);
+            // 
+            // Salvar
+            // 
+            this.Salvar.Enabled = false;
+            this.Salvar.Location = new System.Drawing.Point(96, 130);
+            this.Salvar.Name = "Salvar";
+            this.Salvar.Size = new System.Drawing.Size(75, 23);
+            this.Salvar.TabIndex = 5;
+            this.Salvar.Text = "Salvar";
+            this.Salvar.UseVisualStyleBackColor = true;
+            this.Salvar.Click += new System.EventHandler(this.Salvar_Click);
+            // 
+            // Abrir
+            // 
+            this.Abrir.Enabled = false;
+            this.Abrir.Location = new System.Drawing.Point(177, 130);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(75, 23);
+            this.Abrir.TabIndex = 6;
+            this.Abrir.Text = "Abrir";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.Abrir_Click);
+            // 
+            // Aleatorio
+            // 
+            this.Aleatorio.Enabled = false;
+            this.Aleatorio.Location = new System.Drawing.Point(258, 130);
+            this.Aleatorio.Name = "Aleatorio";
+            this.Aleatorio.Size = new System.Drawing.Size(75, 23);
+            this.Aleatorio.TabIndex = 7;
+            this.Aleatorio.Text = "Aleatorio";
+            this.Aleatorio.UseVisualStyleBackColor = true;
+            this.Aleatorio.Click += new System.EventHandler(this.Aleatorio_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextoMudado);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(84, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextoMudado);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(84, 67);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(258, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.TextoMudado);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(258, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 170);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Aleatorio);
+            this.Controls.Add(this.Abrir);
+            this.Controls.Add(this.Salvar);
+            this.Controls.Add(this.Pasta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gerenciador de desculpas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Pasta;
+        private System.Windows.Forms.Button Salvar;
+        private System.Windows.Forms.Button Abrir;
+        private System.Windows.Forms.Button Aleatorio;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desculpas_de_Bryan
+{
+    public partial class Form1 : Form
+    {
+        private string pasta;
+        private string caminhoEscolhido;
+        private string nomeDoArquivo;
+        private string[] LinhasDoArquivo;
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Pasta_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = caminhoEscolhido;
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pasta = folderBrowserDialog1.SelectedPath;
+                caminhoEscolhido = folderBrowserDialog1.SelectedPath;
+                Salvar.Enabled = true;
+                Abrir.Enabled = true;
+                Aleatorio.Enabled = true;
+            }
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Salvar";
+            saveFileDialog1.Filter = "Text File (*.txt)|*.txt";
+            saveFileDialog1.InitialDirectory = caminhoEscolhido;
+            saveFileDialog1.FileName = textBox1.Text + ".txt";
+
+            if (textBox1.Text != "" || MessageBox.Show("A desculpa esta vazia, você deseja continuar?", "Aviso", MessageBoxButtons.YesNo)==DialogResult.Yes)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    Form1.ActiveForm.Text = "Gerenciador de desculpas";
+                    nomeDoArquivo = saveFileDialog1.FileName;
+                    File.WriteAllText(nomeDoArquivo, textBox1.Text + "\n" + textBox2.Text + "\n" + dateTimePicker1.Text.ToString());
+                }
+            }
+        }
+
+        private void Abrir_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Abrir";
+            saveFileDialog1.InitialDirectory = caminhoEscolhido;
+            openFileDialog1.Filter = "Text File (*.txt)|*.txt";
+
+            if (Form1.ActiveForm.Text == "Gerenciador de desculpas" || MessageBox.Show("A desculpa não foi salva, você deseja continuar?", "Aviso", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    caminhoEscolhido = openFileDialog1.FileName;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+
+                    LinhasDoArquivo = File.ReadAllLines(caminhoEscolhido);
+                    textBox1.Text = LinhasDoArquivo[0];
+                    textBox2.Text = LinhasDoArquivo[1];
+                    textBox3.Text = LinhasDoArquivo[2];
+
+                    Form1.ActiveForm.Text = "Gerenciador de desculpas";
+                }
+            }
+        }
+
+        private void Aleatorio_Click(object sender, EventArgs e)
+        {
+            if (Form1.ActiveForm.Text == "Gerenciador de desculpas" || MessageBox.Show("A desculpa não foi salva, você deseja continuar?", "Aviso", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    var arquivos = Directory.GetFiles(pasta, "*.txt");
+                    var arquivoAleatorio = arquivos[random.Next(0, arquivos.Length)];
+
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox3.Clear();
+
+                    LinhasDoArquivo = File.ReadAllLines(arquivoAleatorio);
+                    textBox1.Text = LinhasDoArquivo[0];
+                    textBox2.Text = LinhasDoArquivo[1];
+                    textBox3.Text = LinhasDoArquivo[2];
+
+                    Form1.ActiveForm.Text = "Gerenciador de desculpas";
+                }catch(Exception erro)
+                {
+                    MessageBox.Show("Houve o erro " + erro, "Erro");
+                }
+
+            }
+        }
+
+        private void TextoMudado(object sender, EventArgs e)
+        {
+            Form1.ActiveForm.Text = "Gerenciador de desculpas*";
+        }
+    }
+}
